ee8114397493123c43094a61cd410995f4e6416e
jiang_xiaofeng <jiang_xiaofeng@cdv.com>
2016-09-27 17:30:21 +0800

    TTt

--- sMonster/src/cn/sController/Dfsdf.java
+++ sMonster/src/cn/sController/Dfsdf.java
@@ -4,14 +4,32 @@ package cn.sController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.sBeans.JsonResult;
+import cn.sBeans.User;
 
 
 @Controller//<bean id="userController" class="userControlelr">
 @RequestMapping("/user")
 public class Dfsdf {
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	@ResponseBody
+	public JsonResult<String> create(@RequestBody User user) {
+		try {
+			String moID = user.getUserName();
+			System.out.println(moID);
+			return new JsonResult<String>(moID).success();
+		} catch (Throwable e) {
+
+			return new JsonResult<String>(null).fail(JsonResult.SERVER_ERROR,
+					e.getMessage());
+		}
+	}
 	@RequestMapping("hello")
 	public String hello(Model model){
 		model.addAttribute("hello", "admin");
